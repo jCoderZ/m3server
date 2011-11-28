4244ee46c1d1ed284c7c5a07dba13cf572d10e1e
mrumpf <michael.rumpf@gmail.com>
2011-11-28 22:36:50 +0000

    Fixed packaging Do not start the Weld Container for now

--- m3server-protocol-upnp/src/main/java/org/jcoderz/m3dditiez/m3server/protocol/upnp/Activator.java
+++ m3server-protocol-upnp/src/main/java/org/jcoderz/m3dditiez/m3server/protocol/upnp/Activator.java
@@ -1,7 +1,5 @@
 package org.jcoderz.m3dditiez.m3server.protocol.upnp;
 
-import org.jboss.weld.environment.se.Weld;
-import org.jboss.weld.environment.se.WeldContainer;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
@@ -10,11 +8,11 @@ public class Activator implements BundleActivator {
 	private static BundleContext context;
 	private static UpnpAdaptor adaptor;
 
-	private static final WeldContainer wc;
+//	private static final WeldContainer wc;
 
 	static {
-		Weld w = new Weld();
-		wc = w.initialize();
+//		Weld w = new Weld();
+//		wc = w.initialize();
 	}
 
 	static BundleContext getContext() {
@@ -28,8 +26,8 @@ public class Activator implements BundleActivator {
 	public void start(BundleContext bundleContext) throws Exception {
 		Activator.context = bundleContext;
 		
-		adaptor = wc.instance().select(UpnpAdaptor.class).get();
-		adaptor.start();
+//		adaptor = wc.instance().select(UpnpAdaptor.class).get();
+//		adaptor.start();
 	}
 
 	/*
@@ -39,7 +37,7 @@ public class Activator implements BundleActivator {
 	public void stop(BundleContext bundleContext) throws Exception {
 		Activator.context = null;
 
-		adaptor.stop();
+//		adaptor.stop();
 	}
 
 }
