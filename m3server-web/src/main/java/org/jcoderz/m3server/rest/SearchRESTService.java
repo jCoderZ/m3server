6c79eb1d68aadc0cbd09061e7716c078f69a0acb
Michael Rumpf <michael@rumpfonline.de>
2012-08-06 22:05:18 +0200

    Implemented dummy mp3 search

--- m3server-web/src/main/java/org/jcoderz/m3server/rest/SearchRESTService.java
+++ m3server-web/src/main/java/org/jcoderz/m3server/rest/SearchRESTService.java
@@ -1,34 +1,34 @@
 package org.jcoderz.m3server.rest;
 
-import java.util.List;
-
 import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
-import org.jcoderz.m3server.model.Member;
+import org.jcoderz.m3server.model.Mp3Info;
 
 /**
  * JAX-RS Example
  * 
- * This class produces a RESTful service to read the contents of the members table.
+ * This class produces a RESTful service to read the contents of the members
+ * table.
  */
 @Path("/search")
 @RequestScoped
 public class SearchRESTService {
 
-   @GET
-   @Produces("application/json")
-   public List<Member> listAllMembers() {
-      return null;
-   }
-
-   @GET
-   @Path("/{id:[0-9][0-9]*}")
-   @Produces("application/json")
-   public Member lookupMemberById(@PathParam("id") long id) {
-      return null;
-   }
+	@GET
+	@Path("/{key}")
+	@Produces("application/json")
+	public Mp3Info lookupMemberById(@PathParam("key") String key) {
+		System.out.println("key=" + key);
+		Mp3Info m3i = new Mp3Info();
+		m3i.setAlbum("Under the blood red sky");
+		m3i.setArtist("U2");
+		m3i.setReleased("1999");
+		m3i.setSize(1000000L);
+		m3i.setTitle("Sundy bloody sunday");
+		return m3i;
+	}
 }
