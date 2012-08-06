aff37e03ec16402da4b261ffe3ef9f2bba0d1d66
Michael Rumpf <michael@rumpfonline.de>
2012-08-06 23:44:50 +0200

    Fixed REST call

--- m3server-web/src/main/webapp/resources/app.js
+++ m3server-web/src/main/webapp/resources/app.js
@@ -2,13 +2,9 @@ $(document).ready(function(){
  
    var search = function(){
         var term = $('#term').val();
-        $.getJSON("http://localhost:8080/m3server-web/rest/search/key/" + term, function(json) {
-            if (json != "Nothing found."){
-            }
-            else {
-               $('#result').html("" + json); 
-            }
-        });
+        var jqxhr = $.getJSON("http://localhost:8080/m3server-web/rest/search/key/" + term, function(json) {
+               $('#result').html("" + JSON.stringify(json)); 
+        }).error(function() {alert("error")});
         return false;
    }
 
