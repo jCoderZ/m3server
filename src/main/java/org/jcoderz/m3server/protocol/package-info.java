843a3f53058295532033989e4b06a98cde30a567
Michael Rumpf <michael@rumpfonline.de>
2012-10-05 11:34:23 +0200

    Cleanup Moved static content into the classpath Added a StaticHttpHandler which delivers content from the classpath Implemented the ProtocolAdapter registration Implemented the MediaRenderer registration

---
+++ src/main/java/org/jcoderz/m3server/protocol/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * The idea is to create an abstraction layer on different remote audio 
+ * playing solutions. The m3server provides a common set of features, which
+ * the renderers must implement.
+ * 
+ * @author mrumpf
+ *
+ */
+package org.jcoderz.m3server.renderer;
