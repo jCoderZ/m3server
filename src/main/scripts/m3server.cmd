3c840718caa613006581eb6e450a64032dde0ebd
mrumpf <mrumpf@W4DE5GSY4167078.m1.sl-si.com>
2012-11-01 13:54:29 +0100

    Added an initial version of the script to launch the m3server under Windows

---
+++ src/main/scripts/m3server.cmd
@@ -0,0 +1,8 @@
+@echo off
+
+set M3_LIBRARY_HOME="%~d0/"
+echo M3_LIBRARY_HOME=%M3_LIBRARY_HOME%
+java -version
+java -DM3_LIBRARY_HOME=%M3_LIBRARY_HOME% -jar tools\lib\m3server-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+
+pause
