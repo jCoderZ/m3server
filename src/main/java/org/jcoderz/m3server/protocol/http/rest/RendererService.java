6dd54c5460fbefc4a928415b67d974f79f495ada
Michael Rumpf <michael@rumpfonline.de>
2012-11-24 14:17:40 +0100

    Removed import

--- src/main/java/org/jcoderz/m3server/protocol/http/rest/RendererService.java
+++ src/main/java/org/jcoderz/m3server/protocol/http/rest/RendererService.java
@@ -5,7 +5,6 @@ import java.util.logging.Logger;
 import javax.ws.rs.Consumes;
 
 import javax.ws.rs.GET;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
